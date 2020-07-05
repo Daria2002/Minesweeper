23222e15cb880dcd0de5b8a4e46a236ba3a9ead0
Daria2002 <daria.matkovic@fer.hr>
2020-07-05 18:59:07 +0200

    Add pop up wnd on button click.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,11 +4,14 @@
 #include <QtWidgets>
 #include <iostream>
 #include <QMainWindow>
+#include <QCoreApplication>
 #include <QApplication>
 #include <QtWidgets>
 #include <QMainWindow>
 #include <QPushButton>
 #include <vector>
+#include <QMessageBox>
+#include <QVBoxLayout>
 #include <QtGui>
 
 MainWindow::MainWindow(QWidget *parent, QLabel* unexposed_label, std::vector<QPushButton*> buttons) :
@@ -17,9 +20,25 @@ MainWindow::MainWindow(QWidget *parent, QLabel* unexposed_label, std::vector<QPu
 {
     ui->setupUi(this);
     std::cout << "Main window constructor\n";
+
+    for(int i = 0; i < buttons.size(); i++) {
+        QString str;
+        str.setNum(i);
+        buttons[i]->setText(str);
+        QObject::connect(buttons[i], SIGNAL(released()), this, SLOT(clickedSlot()));
+    }
+
     Game game(7, 7, 3);
     game.ini(unexposed_label);
     game.start(this, unexposed_label);
+}
+
+void MainWindow::clickedSlot()
+{
+    QMessageBox msgBox;
+    msgBox.setWindowTitle("MessageBox Title");
+    msgBox.setText("You Clicked "+ ((QPushButton*)sender())->text());
+    msgBox.exec();
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -18,8 +18,11 @@ public:
     explicit MainWindow(QWidget *parent = 0, QLabel* unexposed_label = nullptr, std::vector<QPushButton*> buttons = std::vector<QPushButton*>());
     ~MainWindow();
     void execute();
+    void handleButton(QPushButton* m_button = nullptr);
 private:
     Ui::MainWindow *ui;
-};
 
+public slots:
+void clickedSlot();
+};
 #endif // MAINWINDOW_H
