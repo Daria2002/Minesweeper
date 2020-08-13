5ed27e346a029d42434ed849671358f6ea132fa5
Daria2002 <daria.matkovic@fer.hr>
2020-08-13 09:42:05 +0200

    Add some minor changes

--- qrightclickbutton.cpp
+++ qrightclickbutton.cpp
@@ -1,17 +1,12 @@
 #include "qrightclickbutton.h"
-#include "minesweeper.h"
-#include <vector>
-#include <QMessageBox>
 
 QRightClickButton::QRightClickButton(QWidget *parent) : QPushButton(parent) {}
 QRightClickButton::QRightClickButton(QWidget *parent, Game *g) : QPushButton(parent) {
     game = g;
 }
 void right() {
-    std::cout << "right\n";
 }
 void left() {
-    std::cout << "left\n";
 }
 void QRightClickButton::mousePressEvent(QMouseEvent *e) {
     if(e->button() == Qt::RightButton) {
